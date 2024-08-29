84bb4bf85d7e3f0f164373790b4c128ac1476409
boldizsarzoltan <boldizsar.zoltan3@gmail.com>
2024-08-29 07:57:26 +0300

    Finished mapping

--- src/Discount/Application/Service/DiscountOrderMapper.php
+++ src/Discount/Application/Service/DiscountOrderMapper.php
@@ -2,16 +2,76 @@
 
 namespace App\Discount\Application\Service;
 
+use App\Discount\Domain\Model\OrderItem;
+use App\Discount\Domain\Model\OrderItems;
 use App\Discount\Domain\Model\OrderWithDiscount;
+use App\Shared\Settings;
 
 class DiscountOrderMapper
 {
     /**
      * @param OrderWithDiscount $orderWithDiscount
-     * @return array<mixed>
+     * @return array{
+     *     id:int,
+     *     customer-id:int,
+     *     items:array<array{
+     *       product-id:string,
+     *       quantity:int,
+     *       unit-price:float,
+     *       free-count:int,
+     *       total:float
+     *   }>,
+     *     total:float
+     * }
      */
     public function mapOrderWithDiscountToArray(OrderWithDiscount $orderWithDiscount): array
     {
-        return [];
+        return [
+            "id" => $orderWithDiscount->finalOrder->id,
+            "customer-id" => $orderWithDiscount->finalOrder->customer->id,
+            "items" => $this->mapOrderItemsWithDiscountToArray($orderWithDiscount->finalOrder->orderItems),
+            "total" => $orderWithDiscount->finalOrder->orderItems->getItemsTotal() / Settings::PRICE_VALUE_MULTIPLIER
+        ];
+    }
+
+    /**
+     * @param OrderItems<OrderItem> $orderItems
+     * @return array<array{
+     *      product-id:string,
+     *      quantity:int,
+     *      unit-price:float,
+     *      free-count:int,
+     *      total:float
+     *  }>
+     */
+    public function mapOrderItemsWithDiscountToArray(OrderItems $orderItems): array
+    {
+        $result = [];
+        /** @var OrderItem $orderItem */
+        foreach ($orderItems as $orderItem) {
+            $result[] = $this->mapOrderItemWithDiscountToArray($orderItem);
+        }
+        return $result;
+    }
+
+    /**
+     * @param OrderItem $orderItem
+     * @return array{
+     *     product-id:string,
+     *     quantity:int,
+     *     unit-price:float,
+     *     free-count:int,
+     *     total:float
+     * }
+     */
+    private function mapOrderItemWithDiscountToArray(OrderItem $orderItem): array
+    {
+        return [
+            "product-id" => $orderItem->id,
+            "quantity" => $orderItem->quantity,
+            "unit-price" => $orderItem->unitPrice / Settings::PRICE_VALUE_MULTIPLIER,
+            "free-count" => $orderItem->freeCount,
+            "total" => $orderItem->getTotal() / Settings::PRICE_VALUE_MULTIPLIER
+        ];
     }
 }
